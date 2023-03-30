da132d8c89f8c571df229969ebf45d16077a0c72
ysjjovo <ysjjovo@163.com>
2023-03-30 11:34:50 +0800

    base example react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
     </div>
   );
 }
 
-export default App;
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>  
+      <Routes>
+      <Route path="/" element = {<Home />} />
+      <Route path="/about" element = {<About />} />
+      </Routes>
+    </div>
+    </Router>
+  );
+}
+
+export default App
